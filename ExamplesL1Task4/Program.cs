3461bbf63d42147a2340979af734e65e9e61409c
Strannik93 <sergevsc@mail.ru>
2022-10-04 21:53:26 +0600

    Нахождение максимального из 3 чисел

---
+++ ExamplesL1Task4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("Введите 3 числа");
+int x1 = Convert.ToInt32(Console.ReadLine());
+int x2 = Convert.ToInt32(Console.ReadLine());
+int x3 = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+
+if (x1 > x2)
+{max = x1;}
+else
+{max = x2;}
+if (x3>x2) max = x3;
+Console.WriteLine(max);
