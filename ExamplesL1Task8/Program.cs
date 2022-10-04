bfcb8438b591006d0da3ace4a88ba69841679fa5
Strannik93 <sergevsc@mail.ru>
2022-10-04 22:29:57 +0600

    Поиск четных чисел от 1 до заданного

---
+++ ExamplesL1Task8/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("Введите целое число");
+int N = Convert.ToInt32(Console.ReadLine());
+int temp = 0;
+
+if (N < 2) Console.WriteLine("Введите число 2 и более");
+while (temp < N)
+{
+    temp++;
+    if (temp % 2 == 0) Console.Write($"{temp} ");
+}
