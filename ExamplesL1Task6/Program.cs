4160d3fdd896578164e6006210c1f77e9e8b5e9d
Strannik93 <sergevsc@mail.ru>
2022-10-04 22:11:25 +0600

    Четное или нет

---
+++ ExamplesL1Task6/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.WriteLine("Введите целое число");
+int x = Convert.ToInt32(Console.ReadLine());
+
+if (x % 2 == 0) Console.WriteLine("число четное");
+else
+{
+    Console.WriteLine("число не является четным");
+}
