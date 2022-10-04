fdc2e0aadb522c7232dcc49321bb073b4e347183
Strannik93 <sergevsc@mail.ru>
2022-10-04 21:37:35 +0600

    Нахождение максимального и минимального из 2 чисел

---
+++ ExamplesL1Task2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.WriteLine("Введите 2 числа");
+int x1 = Convert.ToInt32(Console.ReadLine());
+int x2 = Convert.ToInt32(Console.ReadLine());
+if(x1 > x2)
+{Console.WriteLine($"{x1} максимальное, {x2} минимальное");}
+else
+{Console.WriteLine($"{x2} максимальное, {x1} минимальное");}
+
