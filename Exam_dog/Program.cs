f6e25fd571c6911f2b22447ff1f2785c5e6638d5
Strannik93 <sergevsc@mail.ru>
2022-10-04 21:18:11 +0600

    Редактирование exam_dog

--- Exam_dog/Program.cs
+++ Exam_dog/Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+int count = 0; 
+int distance = 10000;
+int firstFriendSpeed = 1;
+int secondFriendSpeed = 2;
+int dogSpeed = 5;
+int friend = 2;
+int time = 0;
+
+while (distance > 10)
+    {if (friend == 1)
+    {time = distance/(firstFriendSpeed + dogSpeed);
+    friend = 2;}
+    else
+    {time = distance/(secondFriendSpeed + dogSpeed);
+    friend = 1;}
+distance = distance - (secondFriendSpeed + firstFriendSpeed)*time;
+count++;}
+Console.WriteLine($"Собака пробежит {count} раз");
