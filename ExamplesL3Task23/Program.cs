14820ee4b9fcc2d898a86e309569daeecc02aa11
Strannik93 <sergevsc@mail.ru>
2022-10-14 18:41:15 +0600

    выведение таблицы кубов чисел от 1 до N

---
+++ ExamplesL3Task23/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+if (N < 1) Console.WriteLine("Введите число выше 0");
+while (i < N)
+{
+    i++;
+    Console.Write($"{i*i*i} ");
+}
