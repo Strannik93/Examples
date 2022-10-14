a48ff73ebe04001978c49627a666b98d024ec92f
Strannik93 <sergevsc@mail.ru>
2022-10-14 18:45:21 +0600

    определение суммы чисел в числе

---
+++ ExamplesL4Task27/Program.cs
@@ -0,0 +1,15 @@
+﻿int SumNum(int N)
+{
+    int Sum = 0;
+    for (int i = 0; i < Convert.ToString(N).Length; i++)
+    {
+        Sum = Sum + Convert.ToString(N)[i] - 48;
+    }
+    return Sum;
+}
+
+Console.Clear();
+Console.WriteLine("Введите число");
+int Number = Convert.ToInt32(Console.ReadLine());
+int Rez = SumNum(Number);
+Console.WriteLine(Rez);
