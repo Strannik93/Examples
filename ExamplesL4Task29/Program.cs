2d618778e15dcfc3d9b3c632cf5787b8a3a95c2d
Strannik93 <sergevsc@mail.ru>
2022-10-14 18:48:47 +0600

    Создание массива из 8 элементов и его выведение с отделением 3 последних элементов

---
+++ ExamplesL4Task29/Program.cs
@@ -0,0 +1,42 @@
+﻿void ZapMas (int []Array)
+{
+    for (int i = 0; i < Array.Length; i++)
+    {
+        Console.Write($"Введите {i+1} элемент массива ");
+        Array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+void Ex (int []Array)
+{
+    if (Array.Length > 3) 
+    {
+        Console.Write("[");
+        for (int i = 0; i < Array.Length - 3; i++)
+        {
+            if (i != Array.Length - 4) Console.Write($"{Array[i]},");
+            else Console.Write($"{Array[i]}]");
+        }
+        Console.WriteLine();
+        Console.Write("[");
+        for (int i = Array.Length - 3; i < Array.Length; i++)
+        {
+            if (i != Array.Length - 1) Console.Write($"{Array[i]},");
+            else Console.Write($"{Array[i]}]");
+        }
+    }
+    else
+    {
+       Console.Write("[");
+        for (int i = 0; i < Array.Length; i++)
+        {
+            if (i != Array.Length - 1) Console.Write($"{Array[i]},");
+            else Console.Write($"{Array[i]}]");
+        } 
+    }
+}
+
+Console.Clear();
+int[] Mas = new int[8];
+ZapMas(Mas);
+Ex(Mas);
