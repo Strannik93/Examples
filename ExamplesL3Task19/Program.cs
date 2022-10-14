bad3099adf3d2002d604a9727bb8717400077375
Strannik93 <sergevsc@mail.ru>
2022-10-14 18:35:52 +0600

    проверка 5значного числа на палиндром

---
+++ ExamplesL3Task19/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+Console.WriteLine("Введите пятизначное число");
+int X = Convert.ToInt32(Console.ReadLine());
+int N = Convert.ToString(X).Length;
+if (N != 5 || X < 0) Console.WriteLine("Число не пятизначное или меньше 0");
+else
+{
+    String XK = Convert.ToString(X);
+    int j = N - 1;
+    for (int i = 0; i < j; i++)
+    {
+        if (XK[i] != XK[j])
+        {
+            Console.WriteLine("пятизначное число не является палиндромом");
+            return;
+        }
+        j = j - 1;
+    }
+    Console.WriteLine("пятизначное число является палиндромом");
+}
