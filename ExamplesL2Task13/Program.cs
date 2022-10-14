848c9a9c89119e1c7bd2303b66dc8d48a6bd34d0
Strannik93 <sergevsc@mail.ru>
2022-10-14 18:29:53 +0600

    вывод 3 цифры заданного числа

---
+++ ExamplesL2Task13/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+Console.WriteLine("Введите целое число");
+int x = Convert.ToInt32(Console.ReadLine());
+int N = x.ToString().Length;
+int Nrez = N - 2;
+int temp = 0;
+int x1 = 0;
+if (x < 0) {Console.WriteLine("число меньше 0");}
+else
+{
+    if (N < 3)
+    {Console.WriteLine("третьей цифры нет");}
+    else
+    {
+        while (N > Nrez)
+        {
+            N = N - 1;
+            temp = Convert.ToInt32(Math.Pow(10, N));
+            x1 = x % temp;
+        }
+        N = N - 1;
+        temp = Convert.ToInt32(Math.Pow(10, N));
+        int rez = x1 / temp;
+        Console.WriteLine(rez);
+    }
+}
