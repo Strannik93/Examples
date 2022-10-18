eef4db43f37775109646eb7131e2e39085f73bde
Strannik93 <sergevsc@mail.ru>
2022-10-18 15:25:47 +0600

    количество четных чисел в массиве

---
+++ ExamplesL5Task34/Program.cs
@@ -0,0 +1,21 @@
+﻿int NumEvEl (int[] array)
+{
+    int Sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0) Sum++;
+    }
+    return Sum;
+}
+
+Console.Clear();
+Console.WriteLine("Введите размер массива");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] Mas = new int[N];
+for (int i = 0; i < Mas.Length; i++)
+{
+    Mas[i] = new Random().Next(1,1000);
+    Console.Write(Mas[i] + " ");
+}
+int Rez = NumEvEl(Mas);
+Console.WriteLine("четных чисел = " + Rez);
