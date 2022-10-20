eb3a9d70c8b8c15cc63ded9fa2132ea313b501e0
Strannik93 <sergevsc@mail.ru>
2022-10-21 03:46:55 +0600

    количество чисел больше 0, введенных с клавиатуры

---
+++ ExamplesL6Task41/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите количество чисел ");
+int M = Convert.ToInt32(Console.ReadLine());
+int[] Massive = new int[M];
+int count = 0;
+for (int i = 0; i < Massive.Length; i++)
+{
+    Massive[i] = Convert.ToInt32(Console.ReadLine());
+    if (Massive[i] > 0) count++;
+}
+Console.WriteLine("Чисел больше 0 = " + count);
