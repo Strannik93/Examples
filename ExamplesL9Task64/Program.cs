8ce880b4d24eb9f10e3d7478c526de84ba08910e
Strannik93 <sergevsc@mail.ru>
2022-11-23 20:10:44 +0600

    вывод натуральных чисел от N до 1 с помощью рекурсии

---
+++ ExamplesL9Task64/Program.cs
@@ -0,0 +1,19 @@
+﻿void PrintN (int N)
+{
+    if (N == 1) Console.WriteLine(N);
+    else if (N > 1)
+    {
+        Console.Write(N + ", ");
+        PrintN(N-1);
+    }
+    else
+    {
+        Console.Write($"{N*-1}, ");
+        PrintN((N+1)*-1);
+    }
+}
+
+Console.Clear();
+Console.Write("Введите целое число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+PrintN(N);
