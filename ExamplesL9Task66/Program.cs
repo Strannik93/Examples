95c5061c9dd67ffdfdb2b9cf1ebc17da48f277bd
Strannik93 <sergevsc@mail.ru>
2022-11-23 20:12:33 +0600

    сумма натуральных элементов в промежутке от M до N с помощью рекурсии

---
+++ ExamplesL9Task66/Program.cs
@@ -0,0 +1,16 @@
+﻿int SumMN (int M, int N)
+{
+   if (M < 0 && N >= 0) return SumMN(0,N);
+   else if (M >= 0 && N < 0) return SumMN(M,0);
+   else if (M < 0 && N < 0) return SumMN(0,0);
+   else if (M == N) return N;
+   else if (M > N) return SumMN(N,M);
+   else return N + SumMN(M,N-1);
+}
+
+Console.Clear();
+Console.Write("Введите 1 целое число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2 целое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Сумма натуральных элементов: " + SumMN(m,N));
